0ac28bac1e618dabd8cd54c8969889e3efa78697
Chitos <erna95er@mail.ru>
2025-02-12 21:27:39 +0500

    ahhhh

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
+import "../styles/Reg.css";
 import { Link, useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
