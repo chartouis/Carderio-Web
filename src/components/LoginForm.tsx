ad074c769093d6488f8b3ed42497213011a4a0a7
Chitos <erna95er@mail.ru>
2025-02-12 21:26:48 +0500

    bugfix

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React, { useState } from "react";
-import "../styles/Reg.css";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React, { useState } from "react";
-import "../styles/Reg.css";
 import { Link, useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
